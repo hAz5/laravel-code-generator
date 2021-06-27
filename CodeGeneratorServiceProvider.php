0b5e4ba8700e76377205c989ee754ab248ad6fa2
hAz <claymore0465@gmail.com>
2021-06-27 20:00:02 +0430

    init

---
+++ CodeGeneratorServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\ServiceProvider;
+
+class CodeGeneratorServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__ . '/src/routes.php');
+        $this->loadViewsFrom(__DIR__ . '/src/View');
+    }
+}
---
+++ src/routes.php
@@ -0,0 +1,2 @@
+<?php
+//add routes here
